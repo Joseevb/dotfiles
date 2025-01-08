f70653c58d91a89b5b7c21c0e8b77550e4b37380
Joseevb <joseevasquez04@gmail.com>
2025-01-08 16:00:46 +0100

    added some configurations, must finish setting up the weather config

---
+++ waybar/.config/waybar/scripts/get-current-location.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Fetch location data
+LOCATION_JSON=$(curl -s http://ip-api.com/json?fields=country,city)
+LOCATION_COORDINATES_JSON=$(curl -s http://ip-api.com/json?fields=lat,lon)
+
+LAT=$(echo $LOCATION_COORDINATES_JSON | jq -r .lat)
+LON=$(echo $LOCATION_COORDINATES_JSON | jq -r .lon)
+CITY=$(echo $LOCATION_JSON | jq -r .city)
+ENCODED_CITY=$(echo "$CITY" | sed 's/ /%20/g')
+
+# Handle arguments
+case "$1" in
+    -c|--city)
+        echo $CITY
+        ;;
+    -e|--encoded-city)
+        echo $ENCODED_CITY
+        ;;
+    -lat|--latitude)
+        echo $LAT
+        ;;
+    -lon|--longitude)
+        echo $LON
+        ;;
+    -a|--all)
+        echo "City: $CITY"
+        echo "Latitude: $LAT"
+        echo "Longitude: $LON"
+        ;;
+    *)
+        echo "Usage: $0 {-c|--city | -e|--encoded-city | -lat|--latitude | -lon|--longitude | -a|--all}"
+        exit 1
+        ;;
+esac
