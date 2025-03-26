e1e986dad2b59648b26fe0c911cd377e7a3e6aac
Jose Esteban Vasquez Barboza <joseesteban.vasquez@alita.es>
2025-03-26 09:31:37 +0100

    added README

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,10 +3,10 @@ local highlight_yank_group = vim.api.nvim_create_augroup("highlight_yank", {})
 
 -- Define the autocmd within the augroup
 vim.api.nvim_create_autocmd("TextYankPost", {
-	group = highlight_yank_group,
-	callback = function()
-		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
-	end,
+    group = highlight_yank_group,
+    callback = function()
+        vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
+    end,
 })
 
 -- Create an augroup for prettier
--- nvim/.config/nvim/lua/jose/plugins/code/cmp.lua
+++ nvim/.config/nvim/lua/jose/plugins/code/cmp.lua
@@ -37,8 +37,8 @@ return {
 				ghost_text = true,
 			},
 			mapping = cmp.mapping.preset.insert({
-				["<S-Tab>"] = cmp.mapping.select_prev_item(cmp_select),
-				["<Tab>"] = cmp.mapping.select_next_item(cmp_select),
+				-- ["<S-Tab>"] = cmp.mapping.select_prev_item(cmp_select),
+				-- ["<Tab>"] = cmp.mapping.select_next_item(cmp_select),
 				["<C-d>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(),
